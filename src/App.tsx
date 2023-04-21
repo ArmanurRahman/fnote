0ee24f7ef55f9fe974e5b86dd95e5ba9961711d7
Armanur Rahman <mubeenarman19@gmail.com>
2023-04-21 14:43:51 +0900

    separate esbuild module from app file

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,16 @@
-import { Fragment, useEffect, useRef, useState } from "react";
-import * as esbuild from "esbuild-wasm";
-import { unpkgPathPlugin } from "./plugins/unpkg-path-plugin";
-import { fetchPlugin } from "./plugins/fetch-plugin";
+import { Fragment, useState } from "react";
 import CodeEditor from "./components/code-editor";
 import "bulmaswatch/superhero/bulmaswatch.min.css";
 import Preview from "./components/preview";
+import builder from "./build";
 
 const App = () => {
     const [input, setInput] = useState<string>("");
     const [code, setCode] = useState<string>("");
 
-    const ref = useRef<any>();
-
-    useEffect(() => {
-        startService();
-    }, []);
-    const startService = async () => {
-        const service = await esbuild.startService({
-            worker: true,
-            wasmURL: "https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm",
-        });
-        ref.current = service;
-    };
     const inputSubmitHandler = async () => {
-        if (!ref.current) {
-            return;
-        }
-        // const result = await ref.current.transform(input, {
-        //     loader: "jsx",
-        //     target: "es2015",
-        // });
-
-        const result = await ref.current.build({
-            entryPoints: ["index.js"],
-            bundle: true,
-            write: false,
-            plugins: [unpkgPathPlugin(), fetchPlugin(input)],
-            define: {
-                "process.env.NODE_ENV": '"production"',
-                global: "window",
-            },
-        });
-        // console.log(result);
-        setCode(result.outputFiles[0].text);
+        const result = await builder(input);
+        setCode(result);
     };
 
     const onChangeHandler = (val: string) => {
@@ -52,7 +20,6 @@ const App = () => {
         <Fragment>
             <div>
                 <CodeEditor value={input} onChange={onChangeHandler} />
-
                 <button onClick={inputSubmitHandler}>Submit</button>
             </div>
             <Preview code={code} />
